a2a7d4611a95bd8907319d863d18e37fb78a6f58
‘Juliano <julianoperins@gmail.com>
2020-12-08 14:43:28 -0500

    Requested data from the DB

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
-import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="form">
-      <h1>Crud App</h1>
+  const [movieName, setMovieName] = useState("");
+  const [review, setReview] = useState("");
 
-      <input type="text" name="movieName" />
-      <input type="text" name="review" />
+  const submitReview = () => {
+    axios
+      .post("http://localhost:3001/api/insert", {
+        movieName: movieName,
+        movieReview: review,
+      })
+      .then(() => {
+        alert("Successfully inserted!");
+      });
+
+    setMovieName("");
+    setReview("");
+  };
+
+  console.log(movieName);
+  console.log(review);
+
+  return (
+    <div className="App">
+      <h1>Crud App</h1>
+      <div className="form">
+        <label>Movie Name</label>
+        <input
+          type="text"
+          name="movieName"
+          onChange={(e) => setMovieName(e.target.value)}
+        />
+        <label>Review</label>
+        <input
+          type="text"
+          name="review"
+          onChange={(e) => setReview(e.target.value)}
+        />
+        <button onClick={submitReview}>Submit</button>
+      </div>
     </div>
   );
 }
